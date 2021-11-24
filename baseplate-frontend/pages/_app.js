8702f6f2a76cea55b5285ce49cacd5232d8a76f0
luke-meadows <lukemeadowsdev@gmail.com>
2021-11-24 11:35:29 +0000

    Hooking up Apollo Client

--- baseplate-frontend/pages/_app.js
+++ baseplate-frontend/pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/admin/api',
+  cache: new InMemoryCache(),
+});
+
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default MyApp;
---
+++ baseplate-frontend/pages/_document.js
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  //   static getInitialProps({ renderPage }) {
+  //     const sheet = new ServerStyleSheet();
+  //     const page = renderPage((App) => (props) =>
+  //       sheet.collectStyles(<App {...props} />)
+  //     );
+  //     const styleTags = sheet.getStyleElement();
+  //     return { ...page, styleTags };
+  //   }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
