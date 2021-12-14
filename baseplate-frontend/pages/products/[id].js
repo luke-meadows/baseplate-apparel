b74ef7c9232cd22f660715e6d20840308e8b597e
luke-meadows <lukemeadowsdev@gmail.com>
2021-12-14 20:36:22 +0000

    products

--- baseplate-frontend/pages/products/[id].js
+++ baseplate-frontend/pages/products/[id].js
@@ -1,6 +1,9 @@
 const slug =
   'http://localhost:3000/products/search?brand=nike%20sb&color=black';
 
+const flatspot =
+  'https://www.flatspot.com/collections/clothing/brand_brixton+colour_brown+category_bucket-hats';
+
 export default function Products({ query }) {
   return <div>products {query.id}</div>;
 }
