8d4dca8be9b3123a52a8dd5e5d5e6f188dacf1d5
luke-meadows <lukemeadowsdev@gmail.com>
2021-12-17 11:21:26 +0000

    generateQuery

--- baseplate-frontend/lib/generateQuery.js
+++ baseplate-frontend/lib/generateQuery.js
@@ -1,8 +1,28 @@
 import { gql } from '@apollo/client';
 // brand: 'all', size: 'all', color: 'all', category: 'all'
-export default function genrateQuery(collection, variables) {
-  const queries = {
-    brands: ` allProducts {
+export default function genrateQuery() {
+  const variables = {
+    collection: allBrands,
+    color: 'black',
+    productType: 'shoes',
+    size: 'M',
+    brand: 'Adidas',
+  };
+  const products = {
+    allBrands: ``,
+    shoes: `productCategory_i:"shoes",`,
+    clothes: `productCategory_i:"clothes"`,
+    accessories: `productCategory_i:"accessories"`,
+  };
+  const brandQuery = variables[brand]
+    ? `brand: {brand:"${variables[brand]}",`
+    : '';
+  const typeQuery = variables[productType]
+    ? `productType: {productType:"${variables[productType]}",`
+    : '';
+  // const sizeQuery = variables[brand] ? `brand: {brand:"${variables[brand]}",` : '';
+  const query = ` query INITIAL_STATE_QUERY {
+    allProducts {
       id
       name
       new
@@ -16,7 +36,6 @@ export default function genrateQuery(collection, variables) {
         }
       }
     }
-      `,
-  };
-  return gql`query INITIAL_STATE_QUERY ${queries[collection]}`;
+  }
+    `;
 }
