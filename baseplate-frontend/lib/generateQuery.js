b31d7b6fbc8221bfe0028ee965a5c0fe89dce423
luke-meadows <lukemeadowsdev@gmail.com>
2021-12-18 08:48:26 +0000

    generateQuery

--- baseplate-frontend/lib/generateQuery.js
+++ baseplate-frontend/lib/generateQuery.js
@@ -1,32 +1,33 @@
-import { gql } from '@apollo/client';
-
-export default function generateQuery() {
-  const variables = {
-    collection: 'allBrands',
-    brand: '',
-    productType: '',
-    // color: 'black',
-    // size: 'M',
-  };
+export default function generateQuery(variables) {
+  // const variables = {
+  //   collection: 'clothes',
+  //   brand: '',
+  //   productType: '',
+  //   color: 'black',
+  //   // size: 'M',
+  // };
 
   const products = {
     allBrands: ``,
-    shoes: `productCategory_i:"shoes",`,
-    clothes: `productCategory_i:"clothes",`,
-    accessories: `productCategory_i:"accessories",`,
+    shoes: `productCategory:shoes,`,
+    clothes: `productCategory:clothes,`,
+    accessories: `productCategory:accessories,`,
   };
   const brandQuery = variables.brand
     ? `brand: {brand_i:"${variables.brand}"},`
     : 'brand: {brand_not_contains:"~"},';
 
   const typeQuery = variables.productType
-    ? `productType: {productType:"${variables.productType}"}`
+    ? `productType: {productType:"${variables.productType}"}, `
+    : '';
+  const colorQuery = variables.color
+    ? `color_contains_i: "${variables.color}"`
     : '';
 
   const querySlug = `${
     products[variables.collection]
-  } ${brandQuery} ${typeQuery}`;
-  console.log(querySlug);
+  } ${brandQuery} ${typeQuery} ${colorQuery}`;
+  console.log({ querySlug });
 
   const query = `query DISPLAY_PRODUCTS_QUERY {
     allProducts(where: { ${querySlug} }) {
@@ -45,6 +46,5 @@ export default function generateQuery() {
     }
   }
   `;
-  console.log(query);
   return query;
 }
--- baseplate-frontend/pages/products/[id].js
+++ baseplate-frontend/pages/products/[id].js
@@ -1,10 +1,19 @@
 import { gql, useQuery } from '@apollo/client';
+import { useState } from 'react';
 import generateQuery from '../../lib/generateQuery';
-const PRODUCTS_PAGE_QUERY = gql`
-  ${generateQuery()}
-`;
 
 export default function Products({ query }) {
+  const [queryVariables, setQueryVariables] = useState({
+    collection: 'clothes',
+    brand: '',
+    productType: '',
+    color: 'black',
+    // size: 'M',
+  });
+
+  const PRODUCTS_PAGE_QUERY = gql`
+    ${generateQuery(queryVariables)}
+  `;
   const { data, error, loading } = useQuery(PRODUCTS_PAGE_QUERY);
   console.log({ data, error, loading });
   return <div>products {query.id}</div>;
