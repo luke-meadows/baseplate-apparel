5c510bc747b5140e5c1d3c45ec20fe0b7b2b68df
luke-meadows <lukemeadowsdev@gmail.com>
2021-12-22 17:51:57 +0000

    Size dropdown

--- baseplate-frontend/components/SizeDropdown.js
+++ baseplate-frontend/components/SizeDropdown.js
@@ -1,8 +1,37 @@
 import styled from 'styled-components';
 export default function SizeDropdown() {
   return (
-    <div className="hello">
-      <p>hello</p>
-    </div>
+    <Dropdown>
+      <span class="material-icons-outlined">expand_more</span>
+      <option>Dropdown</option>
+    </Dropdown>
   );
 }
+
+const Dropdown = styled.select`
+  position: relative;
+  font-size: 1.2rem;
+  display: block;
+  margin: 1rem 0;
+  height: 4rem;
+  width: 100%;
+  border: none;
+  text-align: center;
+  outline: none;
+  background: #c8c8c8;
+  text-transform: uppercase;
+  font-family: 'Poppins', sans-serif;
+  color: rgba(0, 0, 0, 0.8);
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none; /* Remove default arrow */
+  &:hover,
+  &:focus {
+    background-color: #02e1f5;
+    cursor: pointer;
+  }
+  span {
+    left: 10px;
+    top: 5px;
+  }
+`;
