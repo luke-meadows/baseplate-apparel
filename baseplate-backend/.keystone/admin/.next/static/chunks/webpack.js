98d8c50fc92acb652fe1e8311c4f67675ba05bb5
luke-meadows <lukemeadowsdev@gmail.com>
2022-01-26 14:27:44 +0000

    remove gitignore files

--- baseplate-backend/.keystone/admin/.next/static/chunks/webpack.js
+++ baseplate-backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "58bb93751d23fbdb137f";
+/******/ 	var hotCurrentHash = "04f38c94fd8062b581b3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
